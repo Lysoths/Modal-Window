5d4afa2f149b271053f2fd1f855f8e8efbb73ef9
Oğuzhan ER <95685721+Lysoths@users.noreply.github.com>
2022-06-08 16:29:30 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const containerMenu = document.querySelectorAll(".container-modal div");
+const closeTag = document.querySelector(".opening .close-tag i");
+const openingMenu = document.querySelector(".opening-modal");
+const opening = document.querySelector(".opening");
+
+containerMenu.forEach((a) => {
+  a.addEventListener("click", () => {
+    openingMenu.classList.toggle("active");
+    opening.classList.toggle("active");
+  });
+});
+
+window.addEventListener("keydown", (a) => {
+  if (a.keyCode == 27) {
+    openingMenu.classList.toggle("active");
+    opening.classList.toggle("active");
+  }
+});
+
+closeTag.addEventListener("click", () => {
+  opening.classList.toggle("active");
+  openingMenu.classList.toggle("active");
+});
+
+openingMenu.addEventListener("click", () => {
+  opening.classList.toggle("active");
+  openingMenu.classList.toggle("active");
+});
